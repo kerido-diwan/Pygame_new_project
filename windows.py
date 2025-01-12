32abd3018def2ea2ba821df0d75c3fcdc6a045c3
kerido-diwan <wows.moomoo@gmail.com>
2025-01-12 21:55:59 +0100

    enrer et sortire d'une maison (non complet)

--- windows.py
+++ windows.py
@@ -30,6 +30,9 @@ class Windows:
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4)
         self.group.add(self.player)
 
+        enter_haus = tmx_data.get_object_by_name("enter_haue")
+        self.enter_haus_rect = pygame.Rect(enter_haus.x , enter_haus.y , enter_haus.width, enter_haus.height)
+
     def handel_input(self):
         pressed = pygame.key.get_pressed()
 
@@ -49,9 +52,70 @@ class Windows:
             self.player.mouv_rigth()
             self.player.change_animation('right')
 
+    def switch_haus(self):
+        # importer et charger la carte (carte_pygame)
+        tmx_data = pytmx.util_pygame.load_pygame('interieur.tmx')
+        map_data = pyscroll.TiledMapData(tmx_data)  # Permet de deplacer la carte dans la fenetre
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data,
+                                                           self.screen.get_size())  # Renu graphique en fonction de la taille de
+        map_layer.zoom = 1.5  # la fenetre avec get_size()
+
+
+        # definir list de colision
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        # dessiner groupe de calces
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4)
+        self.group.add(self.player)
+
+        enter_haus = tmx_data.get_object_by_name("door_exit_haus")
+        self.enter_haus_rect = pygame.Rect(enter_haus.x, enter_haus.y, enter_haus.width, enter_haus.height)
+         #spawn point
+        spawn_haus_point  = tmx_data.get_object_by_name("sapwn_haus")
+        self.player.position[0] = spawn_haus_point.x
+        self.player.position[1] = spawn_haus_point.y
+
+    def switch_world(self):
+        # importer et charger la carte (carte_pygame)
+        tmx_data = pytmx.util_pygame.load_pygame('carte_pygame.tmx')
+        map_data = pyscroll.TiledMapData(tmx_data)  # Permet de deplacer la carte dans la fenetre
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data,
+                                                           self.screen.get_size())  # Renu graphique en fonction de la taille de
+        map_layer.zoom = 1.5  # la fenetre avec get_size()
+
+
+        # definir list de colision
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        # dessiner groupe de calces
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4)
+        self.group.add(self.player)
+
+        enter_haus = tmx_data.get_object_by_name("enter_haue")
+        self.enter_haus_rect = pygame.Rect(enter_haus.x, enter_haus.y, enter_haus.width, enter_haus.height)
+
+         #spawn point
+        spawn_haus_point  = tmx_data.get_object_by_name("haus_exit")
+        self.player.position[0] = spawn_haus_point.x
+        self.player.position[1] = spawn_haus_point.y + 20
 
     def update(self):
         self.group.update()
+        #verifier entre
+        if self.player.feet.colliderect(self.enter_haus_rect):
+            self.switch_haus()
+        # verifier sortie
+        if self.player.feet.colliderect(self.enter_haus_rect):
+            self.switch_world()
+
         #verifier collision
         for sprite in self.group.sprites():
             if sprite.feet.collidelist(self.walls) > -1:
