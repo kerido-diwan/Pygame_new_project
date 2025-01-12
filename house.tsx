1713e353e8ce7aa22ba97381143c5a40751ec716
kerido-diwan <wows.moomoo@gmail.com>
2025-01-12 21:56:11 +0100

    enrer et sortire d'une maison (non complet)

---
+++ house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="house" tilewidth="32" tileheight="32" tilecount="1156" columns="34">
+ <image source=".png" trans="4f546b" width="1088" height="1088"/>
+</tileset>
